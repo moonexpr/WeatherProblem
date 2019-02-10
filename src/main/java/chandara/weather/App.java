b3d90fa140d3f57a4e30148f15ea1ab381a12a0a
John <mrpotatofactory@gmail.com>
2019-02-09 23:38:47 -0600

    Initial Commit

---
+++ src/main/java/chandara/weather/App.java
@@ -0,0 +1,13 @@
+package chandara.weather;
+
+import com.mashape.unirest.http.exceptions.UnirestException;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+    public static void main (String[] args) throws UnirestException {
+        System.out.println ("Hello World!");
+    }
+}
